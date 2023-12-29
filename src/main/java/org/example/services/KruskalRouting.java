693cde9bca21bd332066f100838d901a5ca79944
Unger Daniel <2210859021@fh-burgenland.at>
2023-12-29 22:02:32 +0100

    documentation: add javadoc

--- src/main/java/org/example/services/KruskalRouting.java
+++ src/main/java/org/example/services/KruskalRouting.java
@@ -10,6 +10,8 @@ import java.util.List;
  * This class implements the Kruskal algorithm for finding a minimum spanning tree (MST) in a graph.
  * It processes a list of edges to construct the MST and prints the results.
  *
+ * The algorithm is based on disjoint set data structure for efficient union and find operations.
+ *
  * @author Unger Daniel, Leicht Andreas, Alnahhas Khaled
  * @version 1.0
  */
@@ -37,12 +39,22 @@ public class KruskalRouting {
         printResults();
     }
 
+    /**
+     * Initializes the subsets for each node in the graph.
+     *
+     * @param nodes The number of nodes in the graph.
+     */
     private void generateSubsets(int nodes) {
         for (int i = 0; i < nodes; i++) {
             edgeSubSets[i] = new EdgeSubSet((char) ('A' + i), 0);
         }
     }
 
+    /**
+     * Processes the edges to construct the Minimum Spanning Tree (MST).
+     *
+     * @param edges The list of edges in the graph.
+     */
     private void processEdgesForMST(List<Edge> edges) {
         while (numberOfEdges < edgeSubSets.length - 1) {
             Edge nextEdge = edges.get(counter);
@@ -59,14 +71,20 @@ public class KruskalRouting {
         }
     }
 
+    /**
+     * Performs the union operation on two subsets.
+     *
+     * @param edgeSubsets The array of subsets representing the nodes.
+     * @param x           The root of the first subset.
+     * @param y           The root of the second subset.
+     */
     private void union(EdgeSubSet[] edgeSubsets, char x, char y) {
         char rootX = findRoot(edgeSubsets, x);
         char rootY = findRoot(edgeSubsets, y);
 
         if (edgeSubsets[rootY - 'A'].rank < edgeSubsets[rootX - 'A'].rank) {
             edgeSubsets[rootY - 'A'].parent = rootX;
-        } else if (edgeSubsets[rootX - 'A'].rank
-                < edgeSubsets[rootY - 'A'].rank) {
+        } else if (edgeSubsets[rootX - 'A'].rank < edgeSubsets[rootY - 'A'].rank) {
             edgeSubsets[rootX - 'A'].parent = rootY;
         } else {
             edgeSubsets[rootY - 'A'].parent = rootX;
@@ -74,25 +92,33 @@ public class KruskalRouting {
         }
     }
 
+    /**
+     * Finds the root of the subset to which the given node belongs.
+     *
+     * @param edgeSubSets The array of subsets representing the nodes.
+     * @param i           The node whose root needs to be found.
+     * @return The root of the subset.
+     */
     private char findRoot(EdgeSubSet[] edgeSubSets, char i) {
         if (edgeSubSets[i - 'A'].parent == i)
             return edgeSubSets[i - 'A'].parent;
 
-        edgeSubSets[i - 'A'].parent
-                = findRoot(edgeSubSets, edgeSubSets[i - 'A'].parent);
+        edgeSubSets[i - 'A'].parent = findRoot(edgeSubSets, edgeSubSets[i - 'A'].parent);
         return edgeSubSets[i - 'A'].parent;
     }
 
+    /**
+     * Prints the results of the Minimum Spanning Tree (MST) including edges and total cost.
+     */
     private void printResults() {
-        System.out.println(
-                "Following are the edges of the constructed MST:");
+        System.out.println("Following are the edges of the constructed MST:");
         int minCost = 0;
         for (int i = 0; i < numberOfEdges; i++) {
-            System.out.println(results[i].source + " -- "
-                    + results[i].destination + " == "
+            System.out.println(results[i].source + " -- " + results[i].destination + " == "
                     + ConsoleColor.ANSI_YELLOW + results[i].weight + ConsoleColor.ANSI_RESET);
             minCost += results[i].weight;
         }
         System.out.println("Total cost of MST: " + ConsoleColor.ANSI_GREEN + minCost + ConsoleColor.ANSI_RESET);
     }
 }
+
